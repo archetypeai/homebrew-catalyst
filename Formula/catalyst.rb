b89a230b67a681b02b00ced05eecf748b5a20230
catalyst-release <noreply@archetypeai.team>
2026-03-23 14:07:58 -0400

    fix: use S3 public URLs for tarball downloads (no auth needed)

--- Formula/catalyst.rb
+++ Formula/catalyst.rb
@@ -1,20 +1,14 @@
 class Catalyst < Formula
   desc "Hyperparameter optimization for cortex agents"
   homepage "https://github.com/archetypeai/catalyst"
-  url "https://github.com/archetypeai/catalyst/releases/download/v0.1.0/catalyst_loop-0.1.0.tar.gz",
-      using:    :homebrew_curl,
-      headers: ["Authorization: token #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}",
-                "Accept: application/octet-stream"]
+  url "https://catalyst-releases-archetypeai.s3.us-west-2.amazonaws.com/v0.1.0/catalyst_loop-0.1.0.tar.gz"
   sha256 "a4d0a26802c9ef2a4095fd12983c8ee9f3cf310dc01588ba73d97195e136a9ac"
   license :cannot_represent
 
   depends_on "python@3.12"
 
   resource "cortex-client" do
-    url "https://github.com/archetypeai/catalyst/releases/download/v0.1.0/cortex_client-0.1.0.tar.gz",
-        using:    :homebrew_curl,
-        headers: ["Authorization: token #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}",
-                  "Accept: application/octet-stream"]
+    url "https://catalyst-releases-archetypeai.s3.us-west-2.amazonaws.com/v0.1.0/cortex_client-0.1.0.tar.gz"
     sha256 "2be1c37a92240bc08c7a6d2a5fd43ba05c5d63e79e4acfad17438829c3f61f4d"
   end
 
