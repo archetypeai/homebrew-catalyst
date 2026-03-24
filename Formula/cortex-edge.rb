abc8fd455c2788baae7fc976f2d15cdb98dbd106
catalyst-release <noreply@archetypeai.team>
2026-03-24 00:31:29 +0000

    cortex 0.2.0-rc.7

--- Formula/cortex-edge.rb
+++ Formula/cortex-edge.rb
@@ -1,29 +1,18 @@
 class CortexEdge < Formula
   desc "Build and run cortex agents"
   homepage "https://github.com/archetypeai/catalyst"
-  version "0.2.0-rc.6"
+  version "0.2.0-rc.7"
   license :cannot_represent
 
   if Hardware::CPU.arm?
-    url "https://d9pwqft6ad7vm.cloudfront.net/v0.2.0-rc.6/cortex-darwin-arm64.tar.gz?key=#{ENV["CATALYST_RELEASE_KEY"]}"
-    sha256 "4cdabc89fd5621d694c5894447e39be6f31b1a94d38dbb12a19f13a05dd1719b"
+    url "https://d9pwqft6ad7vm.cloudfront.net/v0.2.0-rc.7/cortex-darwin-arm64.tar.gz?key=eHREC9V2VGP3kzMP4OiWx92694cxgMCq8mNMFbfh6qw"
+    sha256 "c5c5f2be5b4c3e1e1149829433c20c61a585b228c0fa6d4ebd74f8a8c47af326"
   else
-    url "https://d9pwqft6ad7vm.cloudfront.net/v0.2.0-rc.6/cortex-darwin-x86_64.tar.gz?key=#{ENV["CATALYST_RELEASE_KEY"]}"
-    sha256 "15ce3317ede13995d8731368472e84f00f196bbdb8fa6c7843b483dffe65763b"
+    url "https://d9pwqft6ad7vm.cloudfront.net/v0.2.0-rc.7/cortex-darwin-x86_64.tar.gz?key=eHREC9V2VGP3kzMP4OiWx92694cxgMCq8mNMFbfh6qw"
+    sha256 "c2375602cbbff93ed492d9306c54e160bb12564d3a3e4a33fcaad392da64ae43"
   end
 
   def install
-    if ENV["CATALYST_RELEASE_KEY"].nil? || ENV["CATALYST_RELEASE_KEY"].empty?
-      odie <<~EOS
-        CATALYST_RELEASE_KEY is not set. This is required to download release artifacts.
-
-        Get the key:
-          eval "$(scripts/get-release-key.sh)"
-
-        Or add to ~/.zshrc:
-          export CATALYST_RELEASE_KEY=<your-key>
-      EOS
-    end
     bin.install "cortex"
     bin.install_symlink "cortex" => "cx"
   end
