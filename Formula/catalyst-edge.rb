b6aeb09d4563c9a85d54209fe6e98c13096aa55c
catalyst-release <noreply@archetypeai.team>
2026-03-24 15:23:54 +0000

    catalyst 0.2.0-rc.12

--- Formula/catalyst-edge.rb
+++ Formula/catalyst-edge.rb
@@ -1,14 +1,18 @@
 class CatalystEdge < Formula
   desc "Hyperparameter optimization for cortex agents"
   homepage "https://github.com/archetypeai/catalyst"
-  version "0.2.0-rc.11"
+  version "0.2.0-rc.12"
   license :cannot_represent
 
-  url "https://d9pwqft6ad7vm.cloudfront.net/v0.2.0-rc.11/catalyst-darwin-arm64.tar.gz?key=eHREC9V2VGP3kzMP4OiWx92694cxgMCq8mNMFbfh6qw"
-  sha256 "698cc82027965bb27453b040f68a8655aa1f8854db14f4a60e5555b273e51e11"
+  url "https://d9pwqft6ad7vm.cloudfront.net/v0.2.0-rc.12/catalyst-darwin-arm64.tar.gz?key=eHREC9V2VGP3kzMP4OiWx92694cxgMCq8mNMFbfh6qw"
+  sha256 "eb4deb737546253d7a54b61307a726a7cf62c2810e2e4fbafcee8d7b35788f34"
+
+  depends_on "python@3.12"
 
   def install
-    bin.install "catalyst"
+    libexec.install "lib"
+    libexec.install "catalyst" => "catalyst"
+    bin.install_symlink libexec/"catalyst"
   end
 
   test do
