b8f7bc9290cb18dbaba3597d24e2fd7c93396e4c
catalyst-release <noreply@archetypeai.team>
2026-03-24 15:05:55 +0000

    catalyst 0.2.0-rc.11

--- Formula/catalyst-edge.rb
+++ Formula/catalyst-edge.rb
@@ -1,30 +1,14 @@
 class CatalystEdge < Formula
-  include Language::Python::Virtualenv
-
   desc "Hyperparameter optimization for cortex agents"
   homepage "https://github.com/archetypeai/catalyst"
-  url "https://d9pwqft6ad7vm.cloudfront.net/v0.2.0-rc.9/catalyst_loop-0.2.0rc9.tar.gz?key=eHREC9V2VGP3kzMP4OiWx92694cxgMCq8mNMFbfh6qw"
-  sha256 "c8628e865d9f30d03504941318f2223962118c3adaca06f0fc8f766b9b64c2eb"
+  version "0.2.0-rc.11"
   license :cannot_represent
 
-  depends_on "python@3.12"
-
-  resource "cortex-client" do
-    url "https://d9pwqft6ad7vm.cloudfront.net/v0.2.0-rc.9/cortex_client-0.2.0rc9.tar.gz?key=eHREC9V2VGP3kzMP4OiWx92694cxgMCq8mNMFbfh6qw"
-    sha256 "b9705651e2c93a8a26f4e434a50b9322cac7adff2b0500d836876aa969a2b1b6"
-  end
+  url "https://d9pwqft6ad7vm.cloudfront.net/v0.2.0-rc.11/catalyst-darwin-arm64.tar.gz?key=eHREC9V2VGP3kzMP4OiWx92694cxgMCq8mNMFbfh6qw"
+  sha256 "698cc82027965bb27453b040f68a8655aa1f8854db14f4a60e5555b273e51e11"
 
   def install
-    venv = virtualenv_create(libexec, "python3.12")
-    # Install cortex-client first (private dep, not on PyPI)
-    venv.pip_install resource("cortex-client")
-    # Install catalyst-loop with all transitive deps from PyPI
-    system libexec/"bin/pip", "install", "--no-cache-dir", cached_download
-    # Link the CLI entry point
-    (bin/"catalyst").write_env_script(
-      libexec/"bin/catalyst",
-      PATH: "#{libexec}/bin:$PATH",
-    )
+    bin.install "catalyst"
   end
 
   test do
