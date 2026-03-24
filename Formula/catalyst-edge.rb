da3b48f0fcf6b5b3f4cd2e7745704c500e887655
catalyst-release <noreply@archetypeai.team>
2026-03-24 00:35:50 +0000

    catalyst 0.2.0-rc.8

--- Formula/catalyst-edge.rb
+++ Formula/catalyst-edge.rb
@@ -1,15 +1,17 @@
 class CatalystEdge < Formula
+  include Language::Python::Virtualenv
+
   desc "Hyperparameter optimization for cortex agents"
   homepage "https://github.com/archetypeai/catalyst"
-  url "https://d9pwqft6ad7vm.cloudfront.net/v0.2.0-rc.7/catalyst_loop-0.2.0rc7.tar.gz?key=eHREC9V2VGP3kzMP4OiWx92694cxgMCq8mNMFbfh6qw"
-  sha256 "b9062af95283b910d7ee01d74b57cef862cf6eb5c6ab363d9539bb9db6a357c9"
+  url "https://d9pwqft6ad7vm.cloudfront.net/v0.2.0-rc.8/catalyst_loop-0.2.0rc8.tar.gz?key=eHREC9V2VGP3kzMP4OiWx92694cxgMCq8mNMFbfh6qw"
+  sha256 "e50df235f53316649d382c3ac70ba5d81e5af0848566b490216674294ada229d"
   license :cannot_represent
 
   depends_on "python@3.12"
 
   resource "cortex-client" do
-    url "https://d9pwqft6ad7vm.cloudfront.net/v0.2.0-rc.7/cortex_client-0.2.0rc7.tar.gz?key=eHREC9V2VGP3kzMP4OiWx92694cxgMCq8mNMFbfh6qw"
-    sha256 "ccd983b633b1eb842243540eb7aef24427b2da6faa918def7961b8b21277499d"
+    url "https://d9pwqft6ad7vm.cloudfront.net/v0.2.0-rc.8/cortex_client-0.2.0rc8.tar.gz?key=eHREC9V2VGP3kzMP4OiWx92694cxgMCq8mNMFbfh6qw"
+    sha256 "c16ee261331009a1631ada9859e4cacd72b7223afe7558fe7a89554a08209fee"
   end
 
   def install
