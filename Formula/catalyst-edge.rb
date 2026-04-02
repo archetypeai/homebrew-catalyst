d8f90ff74cf9d5f261a3383fdfe9a6e6ca7e2388
catalyst-release <noreply@archetypeai.team>
2026-04-02 13:07:26 +0000

    catalyst 0.2.0-rc.27

--- Formula/catalyst-edge.rb
+++ Formula/catalyst-edge.rb
@@ -1,14 +1,14 @@
 class CatalystEdge < Formula
   desc "Hyperparameter optimization for cortex agents"
   homepage "https://github.com/archetypeai/catalyst"
-  version "0.2.0-rc.26"
+  version "0.2.0-rc.27"
   license :cannot_represent
 
   @@release_key = ENV.fetch("HOMEBREW_CATALYST_RELEASE_KEY") {
     odie "Set HOMEBREW_CATALYST_RELEASE_KEY to install. See: https://github.com/archetypeai/homebrew-catalyst#setup"
   }
-  url "https://d9pwqft6ad7vm.cloudfront.net/v0.2.0-rc.26/catalyst-darwin-arm64.tar.gz?key=#{@@release_key}"
-  sha256 "480f3ab4ac1f3a36a233b12586f74ffd8e77d1a78b217d625fff53f7e5a5112d"
+  url "https://d9pwqft6ad7vm.cloudfront.net/v0.2.0-rc.27/catalyst-darwin-arm64.tar.gz?key=#{@@release_key}"
+  sha256 "fcf92eaa833ed0b5beead429ae69e23205baa1121eba5cd8168c91441d2aef65"
 
   depends_on "python@3.12"
 
